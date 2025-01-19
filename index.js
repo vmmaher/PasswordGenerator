19a92f8bb3cbb67508a37ff8da906c8c4e12fb7e
Vanessa Maher <vanessa.maher@keyin.com>
2025-01-19 17:41:14 -0330

    Introduction of --help argument and change to default length.

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 // Password Generator CLI Application
 // Full Stack JavaScript - QAP 1
 // By: Vanessa Maher
-// Version 0.1.0
+// Version 0.1.1
 
-// First, create code that generates a password without any arguments.
-// const process = require('node:process');
+// Second redition: add onto the base code by adding a --help argument. As well, change length to 8 by default.
+// Length flag addition will be coming next.
+
+const process = require('node:process');
+const arguments = process.argv.slice(2);
+
+if (arguments.includes("--help") || 
+    arguments.includes("--h") ||
+    arguments.includes("help")) {
+    console.log(`
+        This is a generic password generator.
+
+        Arguments:
+        --help, --h, help: Displays this help message.
+    `)
+}
 
 const alphabet = "abcdefghijklmnopqrstuvwxyz";
 let password = "";
 
 // Generate a random alphabetical character until password is 10 characters long
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 8; i++) {
     const randomAlpha = alphabet[Math.floor(Math.random() * alphabet.length)];
     password += randomAlpha;
 }
