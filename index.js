3a66ea90e5b3d225870a1b6ffb2ff7e523fe6395
Vanessa Maher <vanessa.maher@keyin.com>
2025-01-19 18:17:55 -0330

    Added --length argument to specify length of generated password; also added error handling for invalid arguments being passed.

--- index.js
+++ index.js
@@ -1,32 +1,49 @@
 // Password Generator CLI Application
 // Full Stack JavaScript - QAP 1
 // By: Vanessa Maher
-// Version 0.1.1
-
-// Second redition: add onto the base code by adding a --help argument. As well, change length to 8 by default.
-// Length flag addition will be coming next.
+// Version 0.2.0
 
 const process = require('node:process');
 const arguments = process.argv.slice(2);
+const validArguments = ["--length", "--help", "--h", "help"];
+// Set default length to 8 if user does not pass the -- length argument.
+let length = 8;
 
-if (arguments.includes("--help") || 
-    arguments.includes("--h") ||
-    arguments.includes("help")) {
-    console.log(`
-        This is a generic password generator.
+// Arguments:
+if (arguments.includes("--help") || arguments.includes("--h") || arguments.includes("help")) {
+    console.log(`This is a generic password generator.
 
         Arguments:
         --help, --h, help: Displays this help message.
-    `)
+        --length, --l: Sets the length of the password. Default is 8.`);
+        process.exit(0);
+        // Exit after the above so that the code does not continue
+} else if (arguments.includes("--length" || "--l")) {
+    length = arguments[arguments.indexOf("--length") + 1];
+    if (length === undefined || isNaN(length) || length < 1) {
+        // If length is not provided or is not a number, show an error message
+        console.log("Please provide a length value after the --length argument.");    
+    }
+} else {
+    // Check for any invalid arguments 
+    arguments.forEach(arg => {
+        if (arg !== validArguments) {
+            console.log(`Invalid argument: '${arg}'. Please use --help to see the valid arguments.`);
+            process.exit(1);  
+            // Exit with an error after the above so that the code does not continue
+        }
+    });
 }
 
+// Main code:
 const alphabet = "abcdefghijklmnopqrstuvwxyz";
 let password = "";
 
-// Generate a random alphabetical character until password is 10 characters long
-for (let i = 0; i < 8; i++) {
+for (let i = 0; i < length; i++) {
+    // Generate a random alphabetical character until password reaches the correct length
     const randomAlpha = alphabet[Math.floor(Math.random() * alphabet.length)];
     password += randomAlpha;
 }
 
+// Output the password:
 console.log(password)
